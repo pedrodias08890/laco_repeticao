8d14816ab82e52e924d5b0efe9b3db38aeaf4624
Pedro Augusto <pedrodias08890@gmail.com>
2026-03-28 02:18:33 -0300

    Teste laco repetcao

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int resultado = 0;
+        for (int i = 0; i <= 10; i = i + 1) {
+            resultado = i + resultado;
+        }
+        System.out.println(resultado);
+    }
+
+}
